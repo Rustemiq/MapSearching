bedf181cbb169c1dd2bad74056cdae876916ad48
Rustem Kharisov <rustem.renatovich@gmail.com>
2025-03-03 12:21:19 +0500

    Добавлены сразу 10 точек аптек на карту с цветами, соответствующими их графику работы

--- get_organizations.py
+++ get_organizations.py
@@ -5,7 +5,7 @@ api_key = "dda3ddba-c9ea-4ead-9010-f43fbc15c6e3"
 search_api_server = "https://search-maps.yandex.ru/v1/"
 
 
-def get_organizations(address_ll):
+def get_organizations(address_ll, qty):
     search_params = {
         "apikey": api_key,
         "text": "аптека",
@@ -23,5 +23,5 @@ def get_organizations(address_ll):
 
     json_response = response.json()
 
-    organization = json_response["features"][0]
+    organization = json_response["features"][:qty]
     return organization
--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 from get_organizations import get_organizations
 from get_toponym import get_toponym
-from show_information import show_information
 from show_map import show_map
 
 address = input('Адрес -> ')
 toponym = get_toponym(address)
 address_ll = toponym['Point']['pos'].replace(' ', ',')
 
-organization = get_organizations(address_ll)
+organizations = get_organizations(address_ll, 10)
 
-point = organization["geometry"]["coordinates"]
-org_point = f"{point[0]},{point[1]}"
+points = "{0},pmwtm1".format(address_ll)
+for organization in organizations:
+    point = organization["geometry"]["coordinates"]
+    org_point = f"{point[0]},{point[1]}"
 
-org_name = organization["properties"]["CompanyMetaData"]["name"]
-org_address = organization["properties"]["CompanyMetaData"]["address"]
-org_hours = organization["properties"]["CompanyMetaData"]["Hours"]["text"]
-
-pt = "{0},pm2dgl".format(org_point) + '~' + "{0},pmwtm1".format(address_ll)
-show_information(address_ll, org_point, org_name, org_address, org_hours)
-show_map(pt)
+    org_name = organization["properties"]["CompanyMetaData"]["name"]
+    org_address = organization["properties"]["CompanyMetaData"]["address"]
+    try:
+        org_hours = organization["properties"]["CompanyMetaData"]["Hours"]["text"]
+        is_day_and_night = 'круглосуточно' in org_hours
+    except KeyError:
+        is_day_and_night = None
+    if is_day_and_night is None:
+        color = 'gr'
+    elif is_day_and_night:
+        color = 'gn'
+    else:
+        color = 'bl'
+    points += '~' + ("{0},pm2" + color + "m").format(org_point)
+show_map(points)
